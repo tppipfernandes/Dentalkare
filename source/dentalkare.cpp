515216ff5e83ee92c08b402434937563a3b60612
Susana B. Estevao <a48584@ualg.pt>
2014-11-17 16:18:53 +0000

    ALTERACAO DO MENU PRINCIPAL

--- source/dentalkare.cpp
+++ source/dentalkare.cpp
@@ -1,94 +1,94 @@
 #include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <fstream> //inclui a biblioteca fstream, para usar o ofstream e ifstream
 #include "utils.h"
-#include "menu1.h"
-
-
-
-
-
-/**
-* \brief menu 2   
-*
-*/
-void menu2(){
-int op;
-	do{
-		printf("1 - opção 1...\n");
-		printf("2 - opção 2...\n");
-		printf("3 - opção 3...\n");
-		printf("...\n");
-		printf("0 - voltar\n");
-	
-		scanf("d", op);
-	
-		switch(op){
-			case 1: ;break;
-			case 2: ;break;
-			case 3: ;break;
-			case 0: ;break;
-		}
-	}while( op!=0);
-}
-
-
-
+#include "menuPaciente.h"
+#include "menuTratamentos.h"
+#include "menuConsulta.h"
+#include "menuGestao.h"
 
 /**
-* \brief
+* \brief GOTOXY definir coluna e linha
 *
 */
-void menu3(){
-	int op;
-	do{
-		printf("1 - opção 1...\n");
-		printf("2 - opção 2...\n");
-		printf("3 - opção 3...\n");
-		printf("...\n");
-		printf("0 - voltar\n");
-	
-		scanf("d", op);
-	
-		switch(op){
-			case 1: ;break;
-			case 2: ;break;
-			case 3: ;break;
-			case 0: ;break;
-		}
-	}while( op!=0);
-}
-
-
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}                 // Nota: No Turbo-C esta funcao ja esta incluida 
 
 /**
-* \brief
+* \brief menu PRINCIPAL
 *
 */
-void mainmenu(){
-	int op;
+/*
+void mainmenu()
+{	
+	system("cls");
+	system("color f0");
+	NOME n;
+	printf("NOME: ");
+	lerStringComEspacos(n);
+	printf("BEM VINDO AO DENTALKARE, %s\n\n",n);
+	int opcao;	
 	do{
-		printf("1 - opção 1...\n");
-		printf("2 - opção 2...\n");
-		printf("3 - opção 3...\n");
-		printf("...\n");
-		printf("0 - voltar\n");
-	
-		scanf("%d", &op);
-	
-		switch(op){
-			case 1: menu1();break;
-			case 2: menu2();break;
-			case 3: menu3();break;
+		printf("\n\t\tMENU PRINCIPAL");
+		printf("\n\n\t[1] - PACIENTE\n");
+		printf("\n\n\t[2] - TRATAMENTOS\n");
+		printf("\n\n\t[3] - CONSULTA\n");
+		printf("\n\n\t[4] - GESTAO\n");
+		printf("\n\n\t[0] - TERMINAR\n\n");
+		printf("\n\nESCOLHA OPCAO: ");
+		scanf("%d", &opcao);
+		
+		switch(opcao)
+		{
+			case 1: menuPaciente();break;
+			case 2: menuTratamentos(); break;
+			case 3: menuConsulta();break;
+			case 4: menuGestao();break;
 			case 0: return ;break;
 		}
-	}while( op != 0);
+	}while(opcao != 0);
+}
+*/
+
+int main ()
+{
+	system("color f0");
+	NOME n;
+	printf("NOME: ");
+	lerStringComEspacos(n);
+	printf("\n\n================ BEM VINDO AO DENTALKARE, %s ================\n\n",n);
+	getch();
+	//mainmenu();
+	
+	int opcao;	
+	do{
+		system("cls");
+		printf("================ MENU PRINCIPAL ================");
+		printf("\n\n\n\t[1] - PACIENTE\n");
+		printf("\n\n\n\t[2] - TRATAMENTOS\n");
+		printf("\n\n\n\t[3] - CONSULTA\n");
+		printf("\n\n\n\t[4] - GESTAO\n");
+		printf("\n\n\n\t[0] - TERMINAR\n\n");
+		printf("\n\n\nESCOLHA OPCAO: ");
+		scanf("%d", &opcao);
+		
+		switch(opcao)
+		{
+			case 1: menuPaciente();break;
+			case 2: menuTratamentos(); break;
+			case 3: menuConsulta();break;
+			case 4: menuGestao();break;
+			case 0: exit (1);break;
+		}
+	}while(opcao != 0);
+	
+	printf("\n\n\n\tPROGRAMA FINALIZADO!\n\n\t MUITO OBRIGADO POR TER USADO ESTE PROGRAMA");
+	printf("\n\n\nQUALQUER TECLA PARA TERMINAR."); //getch();
 }
 
-
-int main (){
-		NOME n;
-		printf("nome=");
-		lerStringComEspacos(n);
-		printf("Ola %s\n\n",n);
-		mainmenu();
-		printf("bye");
-}
