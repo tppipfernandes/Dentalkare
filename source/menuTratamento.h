d380673aa700e7ef5912e729467cf518402a2ec9
Susana Estevao <a48584@ualg.pt>
2014-12-19 00:21:34 +0000

    Alteracao Final Menu Tratamento

--- source/menuTratamento.h
+++ source/menuTratamento.h
@@ -1,116 +1,92 @@
-/* !
-* \ file
-* \ brief Projeto Modulo Tematico LTIC 2014/2015 "menuTratamento.h"
-* \ details "menuTratamento.h" implementa o menu Tratamento
-* \ author Susana Estevao (a48584)
-* \ date 9 dez 2014
-* \ bug sem erros detetados
-* \ version 0.1
+/**
+* \file
+* \brief Projeto Modulo Tematico LTIC 2014/2015 "menuTratamento.h"
+* \details "menuTratamento.h" implementa o menu dos TRATAMENTOS
+* \date 9 dez 2014
 */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
 #include <conio.h>
-#define N 50 	//Numero maximo de Registos
-#define tratamentoName "tratamentos.txt"
-/**
-* estrututa para armazenar os dados de um TRATAMENTO
-*/
+#include <string.h>
+
+#define N 50
+#define tratamentoName "tratamento.txt"
+
+/** Estrutura para Armazenar os dados dos TRATAMENTOS */
 typedef struct
-{
-	int id; /**< id - inteiro com o numero de identificacao do TRATAMENTO */
+{	int idT; /**< id - inteiro com o numero de identificacao do TRATAMENTO */
 	char tipo[N]; /**< tipo - string com o tipo do TRATAMENTO */
-	float preco; /**< preco - inteiro com o preco do TRATAMENTO */
+	int preco; /**< preco - inteiro com o preco do TRATAMENTO */
 	int estado; /**< estado - inteiro com o estado do TRATAMENTO */
 }tratamento;
-/** define um tipo como sendo um lista de TRATAEMENTOS */
+
+/** Define um tipo como sendo uma listaTratamento */
 typedef tratamento listaTratamento[N];
-listaTratamento t;
+
 /**
-* \brief Funcao que inicializa a estrutura de dados TRATAMENTOS
-* \ param void
-*/
-void initTratamento(listaTratamento t) // funcao para inicializar a estrutura de dados
-{
-    int i;
-    for (i=0; i<N; i++)
-    {
-         t[i].id=0;
-         t[i].estado=0;
-    }
-    return;
-}
-/**
-* \brief Funcao que carrega a estrutura de dados a partir do ficheiro "tratamentos.txt"
+* \brief Funcao que carrega a estrutura de dados a partir do ficheiro "tratamento.txt"
 * \param t : listaTratamento
-* \return void
 */
 void lerTratamento(listaTratamento t)
 {
-	FILE *fileTratamento=fopen(tratamentoName, "r+");
+	FILE *fTratamento=fopen(tratamentoName, "r+");
 	int i;
-	if(fileTratamento==NULL)
+	if(fTratamento==NULL)
 	{
-		printf("\nErro ao abrir ficheiro!");
-		getch(); return;
+		printf("\nErro ao abrir ficheiro! <enter para sair>");
+		getch();
 	}
 	for(i=0; i<N; i++)
 	{
-		fscanf(fileTratamento,"%d", &t[i].id);
-		fscanf(fileTratamento," %[^\n]s%*c", t[i].tipo);
-		fscanf(fileTratamento,"%f", &t[i].preco); 
-		fscanf(fileTratamento,"%d", &t[i].estado);
+		fscanf(fTratamento,"%d", &t[i].idT);
+		fscanf(fTratamento," %[^\n]s%*c", t[i].tipo);
+		fscanf(fTratamento,"%d", &t[i].preco); 
+		fscanf(fTratamento,"%d", &t[i].estado);
 		
 	}
-	//fread(t, sizeof(tratamento), N, fileTratamento);
-	fclose(fileTratamento);
-	printf("\nFicheiro Carregado com Sucesso! <enter para sair>"); getch();
+	fclose(fTratamento);
+	//printf("\nFicheiro Carregado com Sucesso! <enter para continuar>"); 
 }
 /**
-* \brief Funcao que gravar a estrutura de dados no ficheiro "tratamentos.txt"
+* \brief Funcao que gravar a estrutura de dados no ficheiro "tratamento.txt"
 * \param t : listaTratamento
-* \return void
 */
 void gravarTratamento(listaTratamento t)
 {
-	FILE *fileTratamento=fopen(tratamentoName, "w+");
+	FILE *fTratamento=fopen(tratamentoName, "w+");
 	int i;
-	if(fileTratamento==NULL)
+	if(fTratamento==NULL)
 	{
 		printf("\nErro ao gravar ficheiro! <enter para sair>");
-		getch(); return;
+		getch();
 	}
 	for(i=0; i<N; i++)
 	{
 		if(t[i].estado==1)
 		{
-			fprintf(fileTratamento,"%d\n%s\n%.2f\n%d\n\n",
-					t[i].id, t[i].tipo, t[i].preco, t[i].estado);
+			fprintf(fTratamento, "%d\n%s\n%d\n%d\n\n", t[i].idT, t[i].tipo, t[i].preco, t[i].estado);
 		}
-
 	}
-	//fwrite(t, sizeof(tratamento), N, fileTratamento);
-	fclose(fileTratamento);
-	printf("\nFicheiro Gravado com Sucesso! <enter para sair>"); 
-	getch();
+	fclose(fTratamento);
+	//printf("\nFicheiro Gravado com Sucesso! <enter para sair>"); 
 }
 /**
-* \brief Funcao para mostrar todos os TRATAMENTOS gravados em "tratamentos.txt"
+* \brief Funcao para mostrar todos os TRATAMENTOS gravados em "tratamento.txt"
 * \param void
-* \return void
 */
-void mostrarTratamento(listaTratamento t)
+void listarTratamento(listaTratamento t)
 {
 	int i;
 	system("cls");
+	printf("\n-------------------------- LISTAR TRATAMENTOS -------------------------\n");
 	for(i=0; i<N; i++)
 	{
 		if(t[i].estado==1)
 		{
-			printf("Id Tratamento: %d\nTipo de Tratamento: %s\nPreco de Tratamento: %.2f EUROS\nEstado:%d\n\n",
-					t[i].id, t[i].tipo, t[i].preco, t[i].estado);
+			printf("\nId Tratamento: [%d]\nTipo de Tratamento: %s\nPreco de Tratamento: %d EUROS\nEstado:%d\n\n",	t[i].idT, t[i].tipo, t[i].preco, t[i].estado);
 		}
 	}
+	printf("\n------------------------------------------------------------------------\n");
 	printf("\n\nListagem Concluida! <Enter para Continuar>");
 	getch();
 }
@@ -120,24 +96,30 @@ void mostrarTratamento(listaTratamento t)
 */
 void inserirTratamento(listaTratamento t)
 {
-	int i=0, op;
-	system("cls");
+	int i=0, op, op2;
+	
 	do //corre ate o ultimo registo(vai para o primeiro indice onde t[i].estado=0)
 	{
 		i++;
 	}while(t[i].estado>0 && t[i].estado<2);
 	do
 	{
-		printf("\nID Tratamento: %d\n", i);
-		t[i].id=i;
+		system("cls");
+		printf("\n[1]-Inserir Tratamento ou [0]-Sair? "); scanf("%d%*c", &op2);
+		if(op2==0)
+			break;
+		printf("\n----------------------------------------------------\n");
+		printf("\nID Tratamento: [%d]\n", i);
+		t[i].idT=i;
 		printf("Tipo de Tratamento: "); gets(t[i].tipo);
-		printf("Preco de Tratamento: "); scanf("%f%*c", &t[i].preco);
+		printf("Preco de Tratamento: "); scanf("%d%*c", &t[i].preco);
 		t[i].estado=1;
 		i++;
 		while(t[i].estado>0 && t[i].estado<2)
 			i++;
 		printf("\n\nQuer inserir outro tratamento? [1]-SIM [0]-NAO: "); scanf("%d%*c", &op);
 	}while(op==1);
+	getch();
 }
 /**
 * \brief Funcao para alterar um determinado TRATAMENTO
@@ -145,26 +127,26 @@ void inserirTratamento(listaTratamento t)
 */
 int alterarTratamento(listaTratamento t)
 {
-	int i, alt, op, op2, confere;
+	int alt, op, op2, confere;
 	do
 	{
 		system("cls");
 		printf("\nQual o Id do Tratamento para alterar? [0]-SAIR: "); scanf("%d%*c", &alt);
 		if(alt==0) 
 			break;
-		t[alt].id=alt;
-		if(alt==t[alt].id)
+		t[alt].idT=alt;
+		if(alt==t[alt].idT)
 		{
 			printf("\n----------------------------------------------");
-			printf("\nId Tratamento: %d", alt);
+			printf("\nId Tratamento: [%d]", alt);
 			printf("\nTipo de Tratamento: %s", t[alt].tipo);
-			printf("\nPreco de Tratamento: %.2f EUROS", t[alt].preco);
+			printf("\nPreco de Tratamento: %d EUROS", t[alt].preco);
 			printf("\nEstado: %d", t[alt].estado);
 			printf("\n----------------------------------------------");
 		}
 		else
 			printf("\nERRO! Id do Tratamento nao existe.");	
-		printf("\n\nQuer mesmo editar este Tratamento? [1]-SIM [0]-NAO:  "); scanf("%d%*c", &confere);
+		printf("\n\nQuer editar este Tratamento? [1]-SIM [0]-NAO:  "); scanf("%d%*c", &confere);
 		if(confere==1)
 		{
 			printf("\n\nEditar Tratamento ID: [%d]", alt);
@@ -175,21 +157,14 @@ int alterarTratamento(listaTratamento t)
 				printf("\n[2] - Preco do Tratamento");
 				printf("\n[0] - Nenhum");
 				printf("\n\nQual a sua opcao: "); scanf("%d%*c", &op);
-				if(op==1)	
-				{	
-					printf("Tipo de Tratamento: "); gets(t[alt].tipo); 
+				switch(op)
+				{
+					case 1: printf("Tipo de Tratamento: "); gets(t[alt].tipo); break;
+					case 2: printf("Preco do Tratamento: "); scanf("%d%*c", &t[alt].preco); break;
+					case 0: return 0; break;
+					default: printf("\nOpcao Invalida\n"); break;
 				}
-				else
-					if(op==2)	
-					{	
-						printf("Preco do Tratamento: "); scanf("%f", &t[alt].preco); 
-					}
-					else
-						if(op==0) 
-							return 0; 									
-						else
-							printf("\nOpcao Invalida\n");
-			}while(op==0 && op>0 || op<2);
+			}while(op>0 || op<2);
 			t[alt].estado=1;
 			printf("\n\nTratamento alterado com Sucesso!");
 		}
@@ -197,6 +172,7 @@ int alterarTratamento(listaTratamento t)
 			break;
 		printf("\n\nQuer editar outro Tratamento? [1]-SIM [0]-NAO:  ");	scanf("%d%*c", &op2);
 	}while(op2==1);
+	getch();
 }
 /**
 * \brief Funcao para eliminar um determinado TRATAMENTO
@@ -211,17 +187,17 @@ void eliminarTratamento(listaTratamento t)
 		printf("\nQual o Id do Tratamento para eliminar? [0]-Sair: "); scanf("%d",&eli);
 		if(eli==0) 
 			break;
-		t[eli].id=eli;
-		if(eli==t[eli].id)
+		t[eli].idT=eli;
+		if(eli==t[eli].idT)
 		{
 			printf("\n----------------------------------------------");
-			printf("\nId Tratamento: %d", eli);
+			printf("\nId Tratamento: [%d]", eli);
 			printf("\nTipo de Tratamento: %s", t[eli].tipo);
-			printf("\nPreco de Tratamento: %.2f EUROS", t[eli].preco);
+			printf("\nPreco de Tratamento: %d EUROS", t[eli].preco);
 			printf("\nEstado: %d", t[eli].estado);
 			printf("\n----------------------------------------------");
 		}
-		printf("\n\nQuer mesmo eliminar este Tratamento? [1]-SIM [0]-NAO:  "); scanf("%d%*c", &confere);
+		printf("\n\nQuer eliminar este Tratamento? [1]-SIM [0]-NAO:  "); scanf("%d%*c", &confere);
 		if(confere==1)
 		{
 			t[eli].estado=0;
@@ -229,41 +205,44 @@ void eliminarTratamento(listaTratamento t)
 		}
 		printf("\n\nQuer eliminar outro Tratamento? [1]-SIM [0]-NAO:  "); scanf("%d%*c", &op);
 	}while(op==1);
+	getch();
 }
 /**
 * \brief Funcao para mostrar menu TRATAMENTO
 * \param void
-* \return void
 */
 void menuTratamento(void)
 {
 	listaTratamento t;
-	int op;
-	initTratamento(t);
+	char op;
+	int i;
+	//Inicializar estrutura TRATAMENTO
+	for (i=0; i<N; i++)
+    {
+		//t[i].idT=0;
+    	t[i].estado=0;
+    }
+    lerTratamento(t);
 	do
 	{
 		system("cls");
-		printf("\n----- MENU GESTAO TRATAMENTO -------");
-		printf("\n\n[1] - Ler Ficheiro");
-		printf("\n\n[2] - Gravar Ficheiro");
-		printf("\n\n[3] - Inserir Novo Registo");
-		printf("\n\n[4] - Alterar Registo");
-		printf("\n\n[5] - Eliminar Registo");
-		printf("\n\n[6] - Mostrar Todos Registos");
-		printf("\n\n[0] - Voltar Menu Principal");
-		printf("\n-------------------------------------");
-		printf("\n\nQual Opcao: ");	scanf("%d%*c", &op);
+		printf("\n--------------------- MENU GESTAO TRATAMENTO ---------------------");
+		printf("\n\n	[1] - Inserir Novo Registo");
+		printf("\n\n	[2] - Alterar Registo");
+		printf("\n\n	[3] - Eliminar Registo");
+		printf("\n\n	[4] - Mostrar Todos Registos");
+		printf("\n\n	[0] - Voltar Menu Principal");
+		printf("\n------------------------------------------------------------------");
+		printf("\n\nQual Opcao: ");	scanf("%s%*c", &op);
 		switch(op)
 		{
-			case 1: lerTratamento(t); break;
-			case 2: gravarTratamento(t); break;
-			case 3: inserirTratamento(t); break;
-			case 4: alterarTratamento(t); break;
-			case 5: eliminarTratamento(t); break;
-			case 6: mostrarTratamento(t); break;
-			case 0: return; break;
+			case '1': inserirTratamento(t); break;
+			case '2': alterarTratamento(t); break;
+			case '3': eliminarTratamento(t); break;
+			case '4': listarTratamento(t); break;
+			case '0': gravarTratamento(t); return; break;
 			default: printf("\nOpcao Invalida!"); getch(); break;
 		}
-	}while (op!=0);
+	}while (op!='0' || op=='a' || op=='A');
 	getch();
 }
